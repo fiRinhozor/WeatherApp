0ecd221ae07852fca0da7aa6461d066542e2d76c
fiRinhozor <bulatgilfanov0@gmail.com>
2022-07-19 22:17:12 -0700

    Built weather component

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,13 +1,40 @@
 import "./current-weather.css";
 
-const CurrentWeather = ({ weather }) => {
+const CurrentWeather = ({ data }) => {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city">Belgrade</p>
-        <p className="weather-description">Sunny</p>
+        <div>
+          <p className="city">Belgrade</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
       </div>
-      <img alt="weather" className="weather-icon" src="icons/01d.png" />
+
+      <div className="bottom">
+        <p className="temperature">20°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details:</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">22°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">15 hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
