1e97aaa47f6301746baa2f54baf46049fba5ce40
fiRinhozor <bulatgilfanov0@gmail.com>
2022-07-20 18:02:38 -0700

    Accordion imported

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,5 +1,24 @@
-const Forecast = () => {
-  return "Hello";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+
+const Forecast = ({ data }) => {
+  return (
+    <>
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, index) => (
+          <AccordionItem>
+            <AccordionItemHeading></AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
 };
 
 export default Forecast;
